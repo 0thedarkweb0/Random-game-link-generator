0c712c61e9670d33b7e8bc940edd0ef738f26095
0thedarkweb0 <91978458+0thedarkweb0@users.noreply.github.com>
2022-03-21 10:10:24 -0600

    updated links

--- main.js
+++ main.js
@@ -45,7 +45,228 @@
        link == "https://musiclab.chromeexperiments.com/Experiments"
    }
      if (link == 14){
-       link == "https://musiclab.chromeexperiments.com/Experiments"
+       link == "https://webglsamples.org/collectibles/index.html"
    }
+  if (link == 15){
+       link == "https://code.org/"
+   }
+  if (link == 16){
+       link == "http://candy-emperical.appspot.com/#/Prisoners"
+   }
+  if (link == 30){
+       link == "http://corndog.io/"
+   }
+  if (link == 17){
+       link == "https://www.crazycardtrick.com/"
+   }
+  if (link == 18){
+       link == "https://dailykitten.com/"
+   }
+  if (link == 19){
+       link == "https://sites.google.com/site/thegamecompilation/death-run-3d"
+   }
+  if (link == 20){
+       link == "https://games.corusent.com/disney-channel/"
+   }
+  if (link == 21){
+       link == "https://lol.disney.com/games"
+   }
+  if (link == 22){
+       link == "https://www.ducksters.com/games/arcade_games.php"
+   }
+  if (link == 23){
+       link == "http://endless.horse/"
+   }
+  if (link == 24){
+       link == "http://experimonkey.com/games"
+   }
+   if (link == 25){
+       link == "https://www.fandom.com/"
+   }
+   if (link == 26){
+       link == "https://www.spotthedifference.com/explorer.asp?"
+   }
+   if (link == 27){
+       link == "https://flipanim.com/"
+   }
+   if (link == 28){
+       link == "https://www.funbrain.com/games"
+   }
+   if (link == 29){
+       link == "https://turbowarp.org/640417990?fps=60&size=640x360&interpolate&offscreen&stuck&limitless"
+   }
+   if (link == 31){
+       link == "https://scratch.mit.edu/projects/631404563"
+   }
+  if (link == 32){
+       link == "https://scratch.mit.edu/projects/641047011/"
+   }
+  if (link == 33){
+       link == "https://scratch.mit.edu/projects/586531936/fullscreen/"
+   }
+  if (link == 34){
+       link == "https://scratch.mit.edu/projects/628693595/fullscreen/"
+   }
+  if (link == 35){
+       link == "https://www.geo-fs.com/"
+   }
+  if (link == 36){
+       link == "https://www.geoguessr.com/"
+   }
+  if (link == 37){
+       link == "https://turbowarp.org/140185501"
+   }
+  if (link == 38){
+       link == "https://scratch.mit.edu/projects/12411120/"
+   }
+  if (link == 39){
+       link == "https://www.google.com/earth/"
+   }
+  if (link == 40){
+       link == "https://www.google.com/search?q=play+snake"
+   }
+  if (link == 41){
+       link == "https://www.google.com/search?q=play+solitaire"
+   }
+  if (link == 42){
+       link == "https://www.google.com/search?q=tic+tac+toe"
+   }
+ if (link == 43){
+       link == "https://www.google.com/search?surl=1&rlz=1CAJCUZ_enUS904&sxsrf=APq-WBvASeAa3z_jX1Jf8bDXG6UYNrp3ZA:1644546758986&q=minesweeper&spell=1&sa=X&ved=2ahUKEwiljImLzvb1AhWgIUQIHYICDjgQBSgAegQIARAy&biw=1366&bih=697&dpr=1&safe=active&ssui=on"
+   }
+  if (link == 44){
+       link == "https://translate.google.com/"
+   }
+  if (link == 45){
+       link == "https://hackertyper.net/"
+   }
+  if (link == 46){
+       link == "https://heal-em-all-game.netlify.app/"
+   }
+  if (link == 47){
+       link == "https://sites.google.com/alpinesd.org/homework-help-for-school/home"
+   }
+  if (link == 47){
+       link == "https://www.hoodamath.com/"
+   }
+  if (link == 47){
+       link == "http://how-to-waste-your-time.com/"
+   }
+  if (link == 47){
+       link == "https://app.inferkit.com/demo"
+   }
+  if (link == 47){
+       link == "https://atmos.uw.edu/~dargan/EarthGamesUW/InfraredEscape/"
+   }
+  if (link == 47){
+       link == "https://beinternetawesome.withgoogle.com/en_us/interland"
+   }
+  if (link == 47){
+       link == "https://isitchristmas.com/"
+   }
+  if (link == 47){
+       link == "http://www.isithalloweenyet.com/"
+   }
+  if (link == 47){
+       link == "http://gel.msu.edu/isotopolis-game/"
+   }
+  if (link == 47){
+       link == "https://docs.google.com/document/u/0/d/1GnSoIssZL0dMHuo8JqEMNhuP2FjvrRz-i9PZ0WpSbew/edit"
+   }
+  if (link == 47){
+       link == "https://www.arealme.com/jitter-cps-test/en/"
+   }
+  if (link == 47){
+       link == "https://express.adobe.com/page/qGdPObwIKUxLG/"
+   }
+  if (link == 47){
+       link == "https://jspaint.app/"
+   }
+  if (link == 47){
+       link == "https://jummbus.bitbucket.io/"
+   }
+  if (link == 47){
+       link == "https://jump--ashwinbose.repl.co/play.html"
+   }
+  if (link == 47){
+       link == "http://kahoot.it"
+   }
+  if (link == 47){
+       link == "http://kaomoji.ru/en/"
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  if (link == 47){
+       link == ""
+   }
+  
+  
      document.getElementById("result").innerHTML = link;
  };
