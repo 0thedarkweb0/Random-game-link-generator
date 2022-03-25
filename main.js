68a3bc8d6893314930cf8705468aab674084ebcf
0thedarkweb0 <91978458+0thedarkweb0@users.noreply.github.com>
2022-03-25 10:15:12 -0600

    first debug

--- main.js
+++ main.js
@@ -3,406 +3,406 @@
         var link = Math.floor(Math.random() * 133);
 
      if (link == 0){
-        link = "https://abcya.com";
-     }
+        link = "https://abcya.com"
+     };
      if (link == 1){
-        link = "https://airforce.com/airmanchallenge";
-     }
+        link = "https://airforce.com/airmanchallenge"
+     };
      if (link == 2){
-        link = "https://www.blooket.com/stats";
-     }
+        link = "https://www.blooket.com/stats"
+     };
      if (link == 3){
-        link = "https://iamawesome.com";
-     }
+        link = "https://iamawesome.com"
+     };
      if (link == 4){
-        link = "https://turbowarp.org/523967150/fullscreen?hqpen";
-     }
+        link = "https://turbowarp.org/523967150/fullscreen?hqpen"
+     };
      if (link == 5){
-        link = "https://www.autodraw.com";
-     }
+        link = "https://www.autodraw.com"
+     };
      if (link == 6){
-        link = "https://www.awesomeinc.org/core-value-training/webgl/";
-     }
+        link = "https://www.awesomeinc.org/core-value-training/webgl/"
+     };
      if (link == 7){
-        link = "https://buildyourstax.com/";
-     }
-    if (link == 8){
-        link = "http://www.catsinsinks.com/";
-     }
-    if (link == 9){
-        link = "https://www.chess.com/";
-     }
-    if (link == 10){
-        link = "https://chrismckenzie.com/";
-     }
+        link = "https://buildyourstax.com/"
+     };
+     if (link == 8){
+        link = "http://www.catsinsinks.com/"
+     };
+     if (link == 9){
+        link = "https://www.chess.com/"
+     };
+     if (link == 10){
+        link = "https://chrismckenzie.com/"
+     };
      if (link == 11){
-        link = "https://canvas.apps.chrome/";
-     }
-    if (link == 12){
-        link = "https://experiments.withgoogle.com/collection/chrome";
-    }
-   if (link == 13){
-       link = "https://musiclab.chromeexperiments.com/Experiments";
-   }
+        link = "https://canvas.apps.chrome/"
+     };
+     if (link == 12){
+        link = "https://experiments.withgoogle.com/collection/chrome"
+     };
+     if (link == 13){
+       link = "https://musiclab.chromeexperiments.com/Experiments"
+     };
      if (link == 14){
-       link = "https://webglsamples.org/collectibles/index.html";
-   }
-  if (link == 15){
-       link = "https://code.org/";
-   }
-  if (link == 16){
-       link = "http://candy-emperical.appspot.com/#/Prisoners";
-   }
-  if (link == 30){
-       link = "http://corndog.io/";
-   }
-  if (link == 17){
-       link = "https://www.crazycardtrick.com/";
-   }
-  if (link == 18){
-       link = "https://dailykitten.com/";
-   }
-  if (link == 19){
-       link = "https://sites.google.com/site/thegamecompilation/death-run-3d";
-   }
-  if (link == 20){
-       link = "https://games.corusent.com/disney-channel/";
-   }
-  if (link == 21){
-       link = "https://lol.disney.com/games";
-   }
-  if (link == 22){
-       link = "https://www.ducksters.com/games/arcade_games.php";
-   }
-  if (link == 23){
-       link = "http://endless.horse/";
-   }
+       link = "https://webglsamples.org/collectibles/index.html"
+     };
+     if (link == 15){
+       link = "https://code.org/"
+     };
+     if (link == 16){
+       link = "http://candy-emperical.appspot.com/#/Prisoners"
+     };
+     if (link == 30){
+       link = "http://corndog.io/"
+     };
+     if (link == 17){
+       link = "https://www.crazycardtrick.com/"
+     };
+     if (link == 18){
+       link = "https://dailykitten.com/"
+     };
+     if (link == 19){
+       link = "https://sites.google.com/site/thegamecompilation/death-run-3d"
+     };
+     if (link == 20){
+       link = "https://games.corusent.com/disney-channel/"
+     };
+     if (link == 21){
+       link = "https://lol.disney.com/games"
+     };
+     if (link == 22){
+       link = "https://www.ducksters.com/games/arcade_games.php"
+     };
+     if (link == 23){
+       link = "http://endless.horse/"
+   };
   if (link == 24){
-       link = "http://experimonkey.com/games";
-   }
+       link = "http://experimonkey.com/games"
+   };
    if (link == 25){
-       link = "https://www.fandom.com/";
-   }
+       link = "https://www.fandom.com/"
+   };
    if (link == 26){
-       link = "https://www.spotthedifference.com/explorer.asp?";
-   }
+       link = "https://www.spotthedifference.com/explorer.asp?"
+   };
    if (link == 27){
-       link = "https://flipanim.com/";
-   }
+       link = "https://flipanim.com/"
+   };
    if (link == 28){
-       link = "https://www.funbrain.com/games";
-   }
+       link = "https://www.funbrain.com/games"
+   };
    if (link == 29){
-       link = "https://turbowarp.org/640417990?fps=60&size=640x360&interpolate&offscreen&stuck&limitless";
-   }
+       link = "https://turbowarp.org/640417990?fps=60&size=640x360&interpolate&offscreen&stuck&limitless"
+   };
    if (link == 31){
-       link = "https://scratch.mit.edu/projects/631404563";
-   }
+       link = "https://scratch.mit.edu/projects/631404563"
+   };
   if (link == 32){
-       link = "https://scratch.mit.edu/projects/641047011/";
-   }
+       link = "https://scratch.mit.edu/projects/641047011/"
+   };
   if (link == 33){
-       link = "https://scratch.mit.edu/projects/586531936/fullscreen/";
-   }
+       link = "https://scratch.mit.edu/projects/586531936/fullscreen/"
+   };
   if (link == 34){
-       link = "https://scratch.mit.edu/projects/628693595/fullscreen/";
-   }
+       link = "https://scratch.mit.edu/projects/628693595/fullscreen/"
+   };
   if (link == 35){
-       link = "https://www.geo-fs.com/";
-   }
+       link = "https://www.geo-fs.com/"
+   };
   if (link == 36){
-       link = "https://www.geoguessr.com/";
-   }
+       link = "https://www.geoguessr.com/"
+   };
   if (link == 37){
-       link = "https://turbowarp.org/140185501";
-   }
+       link = "https://turbowarp.org/140185501"
+   };
   if (link == 38){
-       link = "https://scratch.mit.edu/projects/12411120/";
-   }
+       link = "https://scratch.mit.edu/projects/12411120/"
+   };
   if (link == 39){
-       link = "https://www.google.com/earth/";
-   }
+       link = "https://www.google.com/earth/"
+   };
   if (link == 40){
-       link = "https://www.google.com/search?q=play+snake";
-   }
+       link = "https://www.google.com/search?q=play+snake"
+   };
   if (link == 41){
-       link = "https://www.google.com/search?q=play+solitaire";
-   }
+       link = "https://www.google.com/search?q=play+solitaire"
+   };
   if (link == 42){
-       link = "https://www.google.com/search?q=tic+tac+toe";
-   }
+       link = "https://www.google.com/search?q=tic+tac+toe"
+   };
  if (link == 43){
-       link = "https://www.google.com/search?surl=1&rlz=1CAJCUZ_enUS904&sxsrf=APq-WBvASeAa3z_jX1Jf8bDXG6UYNrp3ZA:1644546758986&q=minesweeper&spell=1&sa=X&ved=2ahUKEwiljImLzvb1AhWgIUQIHYICDjgQBSgAegQIARAy&biw=1366&bih=697&dpr=1&safe=active&ssui=on";
-   }
+       link = "https://www.google.com/search?surl=1&rlz=1CAJCUZ_enUS904&sxsrf=APq-WBvASeAa3z_jX1Jf8bDXG6UYNrp3ZA:1644546758986&q=minesweeper&spell=1&sa=X&ved=2ahUKEwiljImLzvb1AhWgIUQIHYICDjgQBSgAegQIARAy&biw=1366&bih=697&dpr=1&safe=active&ssui=on"
+   };
   if (link == 44){
-       link = "https://translate.google.com/";
-   }
+       link = "https://translate.google.com/"
+   };
   if (link == 45){
-       link = "https://hackertyper.net/";
-   }
+       link = "https://hackertyper.net/"
+   };
   if (link == 46){
-       link = "https://heal-em-all-game.netlify.app/";
-   }
+       link = "https://heal-em-all-game.netlify.app/"
+   };
   if (link == 47){
-       link = "https://sites.google.com/alpinesd.org/homework-help-for-school/home";
-   }
+       link = "https://sites.google.com/alpinesd.org/homework-help-for-school/home"
+   };
   if (link == 48){
-       link = "https://www.hoodamath.com/";
-   }
+       link = "https://www.hoodamath.com/"
+   };
   if (link == 49){
-       link = "http://how-to-waste-your-time.com/";
-   }
+       link = "http://how-to-waste-your-time.com/"
+   };
   if (link == 50){
-       link = "https://app.inferkit.com/demo";
-   }
+       link = "https://app.inferkit.com/demo"
+   };
   if (link == 51){
-       link = "https://atmos.uw.edu/~dargan/EarthGamesUW/InfraredEscape/";
-   }
+       link = "https://atmos.uw.edu/~dargan/EarthGamesUW/InfraredEscape/"
+   };
   if (link == 52){
-       link = "https://beinternetawesome.withgoogle.com/en_us/interland";
-   }
+       link = "https://beinternetawesome.withgoogle.com/en_us/interland"
+   };
   if (link == 53){
-       link = "https://isitchristmas.com/";
-   }
+       link = "https://isitchristmas.com/"
+   };
   if (link == 54){
-       link = "http://www.isithalloweenyet.com/";
-   }
+       link = "http://www.isithalloweenyet.com/"
+   };
   if (link == 55){
-       link = "http://gel.msu.edu/isotopolis-game/";
-   }
+       link = "http://gel.msu.edu/isotopolis-game/"
+   };
   if (link == 56){
-       link = "https://docs.google.com/document/u/0/d/1GnSoIssZL0dMHuo8JqEMNhuP2FjvrRz-i9PZ0WpSbew/edit";
-   }
+       link = "https://docs.google.com/document/u/0/d/1GnSoIssZL0dMHuo8JqEMNhuP2FjvrRz-i9PZ0WpSbew/edit"
+   };
   if (link == 57){
-       link = "https://www.arealme.com/jitter-cps-test/en/";
-   }
+       link = "https://www.arealme.com/jitter-cps-test/en/"
+   };
   if (link == 58){
-       link = "https://express.adobe.com/page/qGdPObwIKUxLG/";
-   }
+       link = "https://express.adobe.com/page/qGdPObwIKUxLG/"
+   };
   if (link == 59){
-       link = "https://jspaint.app/";
-   }
+       link = "https://jspaint.app/"
+   };
   if (link == 60){
-       link = "https://jummbus.bitbucket.io/";
-   }
+       link = "https://jummbus.bitbucket.io/"
+   };
   if (link == 61){
-       link = "https://jump--ashwinbose.repl.co/play.html";
-   }
+       link = "https://jump--ashwinbose.repl.co/play.html"
+   };
   if (link == 62){
-       link = "http://kahoot.it";
-   }
+       link = "http://kahoot.it"
+   };
   if (link == 63){
-       link = "http://kaomoji.ru/en/";
-   }
+       link = "http://kaomoji.ru/en/"
+   };
   if (link == 64){
-       link = "http://www.kittenwar.com/";
-   }
+       link = "http://www.kittenwar.com/"
+   };
   if (link == 65){
-       link = "http://koalastothemax.com/";
-   }
+       link = "http://koalastothemax.com/"
+   };
   if (link == 66){
-       link = "https://www.linerider.com/";
-   }
+       link = "https://www.linerider.com/"
+   };
   if (link == 67){
-       link = "https://www.lego.com/en-us/kids/games";
-   }
+       link = "https://www.lego.com/en-us/kids/games"
+   };
   if (link == 68){
-       link = "https://littlealchemy.com/";
-   }
+       link = "https://littlealchemy.com/"
+   };
   if (link == 69){
-       link = "https://www.youtube.com/watch?v=Yw7LCJixG1E";
-   }
+       link = "https://www.youtube.com/watch?v=Yw7LCJixG1E"
+   };
   if (link == 70){
-       link = "https://littlealchemy2.com/";
-   }
+       link = "https://littlealchemy2.com/"
+   };
   if (link == 71){
-       link = "https://www.lumpty.com/amusements/Games/games.html";
-   }
+       link = "https://www.lumpty.com/amusements/Games/games.html"
+   };
   if (link == 72){
-       link = "https://www.mathsisfun.com/games/";
-   }
+       link = "https://www.mathsisfun.com/games/"
+   };
   if (link == 73){
-       link = "https://arcade.makecode.com/";
-   }
+       link = "https://arcade.makecode.com/"
+   };
   if (link == 74){
-       link = "https://pramuwaskito.org/game/mario/mario.html";
-   }
+       link = "https://pramuwaskito.org/game/mario/mario.html"
+   };
   if (link == 75){
-       link = "https://www.mathplayground.com/";
-   }
+       link = "https://www.mathplayground.com/"
+   };
   if (link == 76){
-       link = "http://moodlight.pro/?";
-   }
+       link = "http://moodlight.pro/?"
+   };
   if (link == 77){
-       link = "https://www.nationstates.net/";
-   }
+       link = "https://www.nationstates.net/"
+   };
   if (link == 78){
-       link = "http://neal.fun/";
-   }
+       link = "http://neal.fun/"
+   };
   if (link == 79){
-       link = "https://www.nitrotype.com/";
-   }
+       link = "https://www.nitrotype.com/"
+   };
   if (link == 80){
-       link = "http://personal.denison.edu/~lalla/314s19/midsemester/nightdrivingsimulator/index.html";
-   }
+       link = "http://personal.denison.edu/~lalla/314s19/midsemester/nightdrivingsimulator/index.html"
+   };
   if (link == 81){
-       link = "https://osrs-idle-clicker.netlify.app/";
-   }
+       link = "https://osrs-idle-clicker.netlify.app/"
+   };
   if (link == 82){
-       link = "https://jamesfriend.com.au/pce-js/mecc/oregon-trail.html";
-   }
+       link = "https://jamesfriend.com.au/pce-js/mecc/oregon-trail.html"
+   };
   if (link == 83){
-       link = "http://particle-clicker.web.cern.ch/";
-   }
+       link = "http://particle-clicker.web.cern.ch/"
+   };
   if (link == 84){
-       link = "https://pbskids.org/";
+       link = "https://pbskids.org/"
    
   if (link == 85){
-       link = "https://www.peta.org/features/games/";
-   }
+       link = "https://www.peta.org/features/games/"
+   };
   if (link == 86){
-       link = "https://www.pictureofhotdog.com/home-margot";
-   }
+       link = "https://www.pictureofhotdog.com/home-margot"
+   };
   if (link == 87){
-       link = "http://pixelsfighting.com/";
-   }
+       link = "http://pixelsfighting.com/"
+   };
    if (link == 88){
-       link = "https://www.pixilart.com/draw";
-   }
+       link = "https://www.pixilart.com/draw"
+   };
    if (link == 89){
-       link = "https://pixlr.com/x/";
-   }
+       link = "https://pixlr.com/x/"
+   };
    if (link == 90){
-       link = "http://www.slightlyinteresting.com/plasmaglobe/plasma.asp";
-   }
+       link = "http://www.slightlyinteresting.com/plasmaglobe/plasma.asp"
+   };
    if (link == 91){
-       link = "https://www.poptropica.com/";
-   }
+       link = "https://www.poptropica.com/"
+   };
    if (link == 92){
-       link = "https://play.prodigygame.com/";
-   }
+       link = "https://play.prodigygame.com/"
+   };
    if (link == 93){
-       link = "https://proxx.netlify.app/";
-   }
+       link = "https://proxx.netlify.app/"
+   };
    if (link == 94){
-       link = "https://quickdraw.withgoogle.com/";
-   }
+       link = "https://quickdraw.withgoogle.com/"
+   };
    if (link == 95){
-       link = "http://randomcolour.com/";
-   }
+       link = "http://randomcolour.com/"
+   };
    if (link == 96){
-       link = "http://www.randomkittengenerator.com/";
-   }
+       link = "http://www.randomkittengenerator.com/"
+   };
    if (link == 97){
-       link = "https://replit.com/apps/kajam2021";
-   }
+       link = "https://replit.com/apps/kajam2021"
+   };
    if (link == 98){
-       link = "https://rogue-carrier--lerich.repl.co/";
-   }
+       link = "https://rogue-carrier--lerich.repl.co/"
+   };
    if (link == 99){
-       link = "https://www.safekidgames.com/";
-   }
+       link = "https://www.safekidgames.com/"
+   };
    if (link == 100){
-       link = "http://castle.chirpingmustard.com/castle.html";
-   }
+       link = "http://castle.chirpingmustard.com/castle.html"
+   };
    if (link == 101){
-       link = "https://santatracker.google.com/";
-   }
+       link = "https://santatracker.google.com/"
+   };
    if (link == 102){
-       link = "https://sites.google.com/site/satznstuff/home";
-   }
+       link = "https://sites.google.com/site/satznstuff/home"
+   };
    if (link == 103){
-       link = "https://htwins.net/scale2/";
-   }
+       link = "https://htwins.net/scale2/"
+   };
    if (link == 104){
-       link = "https://scratch.mit.edu/";
-   }
+       link = "https://scratch.mit.edu/"
+   };
    if (link == 105){
-       link = "https://www.sesamestreet.org/games";
-   }
+       link = "https://www.sesamestreet.org/games"
+   };
     if (link == 106){
-       link = "https://scratch.mit.edu/studios/31000506";
-   }
+       link = "https://scratch.mit.edu/studios/31000506"
+   };
     if (link == 107){
-       link = "https://playset.netlify.app/";
-   }
+       link = "https://playset.netlify.app/"
+   };
     if (link == 108){
-       link = "https://pramuwaskito.org/game/shootball/";
-   }
+       link = "https://pramuwaskito.org/game/shootball/"
+   };
     if (link == 109){
-       link = "https://sh.9pfs.repl.co/";
-   }
+       link = "https://sh.9pfs.repl.co/"
+   };
     if (link == 110){
-       link = "https://turbowarp.org/601692509/fullscreen";
-   }
+       link = "https://turbowarp.org/601692509/fullscreen"
+   };
     if (link == 111){
-       link = "https://sketch.io/sketchpad/";
-   }
+       link = "https://sketch.io/sketchpad/"
+   };
     if (link == 112){
-       link = "https://www.sporcle.com/";
-   }
+       link = "https://www.sporcle.com/"
+   };
     if (link == 113){
-       link = "https://makecode.com/_2fJUCRRva8A2";
-   }
+       link = "https://makecode.com/_2fJUCRRva8A2"
+   };
     if (link == 114){
-       link = "https://www.mathsisfun.com/games/tetris.html";
-   }
+       link = "https://www.mathsisfun.com/games/tetris.html"
+   };
     if (link == 115){
-       link = "https://docs.google.com/document/d/1Rvn3ASm7-hUGg7wwiDP6pfzLIkeX380t8TN12XLtR7o/edit";
-   }
+       link = "https://docs.google.com/document/d/1Rvn3ASm7-hUGg7wwiDP6pfzLIkeX380t8TN12XLtR7o/edit"
+   };
     if (link == 116){
-       link = "https://the-price-of-adventure-game-jam--carlosalbertoalberto.repl.co/";
-   }
+       link = "https://the-price-of-adventure-game-jam--carlosalbertoalberto.repl.co/"
+   };
     if (link == 117){
-       link = "https://thisissand.com/";
-   }
+       link = "https://thisissand.com/"
+   };
     if (link == 118){
-       link = "https://www.childline.org.uk/toolbox/";
-   }
+       link = "https://www.childline.org.uk/toolbox/"
+   };
     if (link == 119){
-       link = "https://games.corusent.com/treehouse/";
-   }
+       link = "https://games.corusent.com/treehouse/"
+   };
     if (link == 120){
-       link = "https://turbowarp.org/";
-   }
+       link = "https://turbowarp.org/"
+   };
     if (link == 121){
-       link = "https://ig.ft.com/uber-game/";
-   }
+       link = "https://ig.ft.com/uber-game/"
+   };
     if (link == 122){
-       link = "https://sites.google.com/site/thegamecompilation/home";
-   }
+       link = "https://sites.google.com/site/thegamecompilation/home"
+   };
     if (link == 123){
-       link = "https://www.onlinepianist.com/virtual-piano";
-   }
+       link = "https://www.onlinepianist.com/virtual-piano"
+   };
     if (link == 124){
-       link = "http://weavesilk.com/";
-   }
+       link = "http://weavesilk.com/"
+   };
     if (link == 125){
-       link = "https://makecode.com/_Mk7dM4eKwLsW";
-   }
+       link = "https://makecode.com/_Mk7dM4eKwLsW"
+   };
     if (link == 126){
-       link = "https://www.whatsmystarbucksname.com/";
-   }
+       link = "https://www.whatsmystarbucksname.com/"
+   };
     if (link == 127){
-       link = "https://r8.whiteboardfox.com/";
-   }
+       link = "https://r8.whiteboardfox.com/"
+   };
     if (link == 128){
-       link = "https://www.nytimes.com/games/wordle/index.html";
-   }
+       link = "https://www.nytimes.com/games/wordle/index.html"
+   };
     if (link == 129){
-       link = "https://youtube.com/";
-   }
+       link = "https://youtube.com/"
+   };
     if (link == 130){
-       link = "https://games.corusent.com/ytv/";
-   }
+       link = "https://games.corusent.com/ytv/"
+   };
     if (link == 131){
-       link = "https://15puzzle.netlify.app/";
-   }
+       link = "https://15puzzle.netlify.app/"
+   };
     if (link == 132){
-       link = "https://www.mathsisfun.com/games/2048.html";
-   }
+       link = "https://www.mathsisfun.com/games/2048.html"
+   };
     
   
   
      document.getElementById("result").innerHTML = link;
- }
+ };
